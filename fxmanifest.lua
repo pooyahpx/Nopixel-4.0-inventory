5bdb792e7720a5adeca98cbf466b9a5c28abbe32
pooyahpx <73234330+pooyahpx@users.noreply.github.com>
2024-04-19 03:24:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,13 +11,14 @@ shared_scripts {
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+	'server/main.lua',
+	-- 'server/visual.lua',
 }
 
 client_scripts {
 	'client/main.lua',
-	'client/visual.lua',
 }
+
 
 ui_page {
 	'html/ui.html'
@@ -27,16 +28,15 @@ files {
 	'html/ui.html',
 	'html/css/main.css',
 	'html/js/app.js',
-	'html/inventory_images/body.png',
 	'html/images/*.svg',
 	'html/images/*.png',
 	'html/images/*.jpg',
+	'html/inventory_images/*.png',
 	'html/ammo_images/*.png',
 	'html/attachment_images/*.png',
 	'html/*.ttf'
 }
 
 lua54 'yes'
-
 
 ------ dev BY POOYA --------
