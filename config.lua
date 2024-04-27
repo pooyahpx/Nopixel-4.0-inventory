3568521459fa18052e0060c79dcaed361116ea8e
pooyahpx <73234330+pooyahpx@users.noreply.github.com>
2024-04-27 20:31:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,8 +2,8 @@ Config = {}
 
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
-Config.MaxInventoryWeight = 250000 -- Max weight a player can carry (default 250kg, written in grams)
-Config.MaxInventorySlots = 41 -- Max inventory slots for a player
+Config.MaxInventoryWeight = 160000 -- Max weight a player can carry (default 120kg, written in grams)
+Config.MaxInventorySlots = 15 -- Max inventory slots for a player
 
 Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
 Config.MaxDropViewDistance = 12.5 -- The distance in GTA Units that a drop can be seen
@@ -385,20 +385,25 @@ Config.MaximumAmmoValues = {
     ["rifle"] = 250,
 }
 
-
 Config.DropItems = {
     weed_skunk = `prop_stockade_wheel_flat`,
     lockpick = `v_res_tre_plant`,
     jerry_can = `prop_jerrycan_01a`,
-    weapon_dagger = `prop_w_me_dagger`,
-    weapon_bat = `p_cs_bbbat_01`,
-    weapon_bottle = `prop_w_me_bottle`,
-    weapon_crowbar = `prop_ing_crowbar`,
+
+
+      --Slagvåpen
+      weapon_dagger = `prop_w_me_dagger`,
+      weapon_bat = `p_cs_bbbat_01`,
+      weapon_bottle = `prop_w_me_bottle`,
+      weapon_crowbar = `prop_ing_crowbar`,
+    --   weapon_flashlight = `w_pi_pistol_mag1`,
     weapon_golfclub = `prop_golf_iron_01`,
     weapon_hammer = `prop_tool_hammer`,
     weapon_hatchet = `prop_w_me_hatchet`,
+    -- weapon_knuckle = `w_pi_pistol_mag1`,
     weapon_knife = `prop_cs_bowie_knife`,
     weapon_machete = `prop_ld_w_me_machette`,
+    -- weapon_switchblade = `w_pi_pistol_mag1`,
     weapon_nightstick = `w_me_nightstick`,
     weapon_wrench = `prop_tool_wrench`,
     weapon_battleaxe = `prop_tool_fireaxe`,
@@ -408,6 +413,8 @@ Config.DropItems = {
     weapon_garbagebag = `prop_rub_binbag_sd_01`,
     weapon_handcuffs = `prop_cs_cuffs_01`,
     weapon_bread = `v_ret_247_bread1`,
+
+    --handguns
     weapon_pistol = `w_pi_combatpistol_luxe`,
     weapon_pistol_mk2 = `weapon_pistol_mk2`,
     weapon_combatpistol = `w_pi_combatpistol`,
@@ -417,6 +424,8 @@ Config.DropItems = {
     weapon_heavypistol = `w_pi_heavypistol`,
     weapon_vintagepistol = `w_pi_vintage_pistol`,
     weapon_revolver = `w_pi_heavypistol`,
+    
+    --submachine guns
     weapon_microsmg = `w_sb_microsmg`,
     weapon_smg = `w_sb_smg`,
     weapon_smg_mk2 = `w_sb_smg`,
@@ -424,26 +433,40 @@ Config.DropItems = {
     weapon_combatpdw = `w_sb_smg`,
     weapon_machinepistol = `w_pi_appistol`,
     weapon_minismg = `w_sb_smg`,
+   
+    --shotguns
     weapon_pumpshotgun = `w_sg_pumpshotgun`,
     weapon_sawnoffshotgun = `w_sg_sawnoff`,
     weapon_musket = `w_ar_musket`,
     weapon_dbshotgun = `w_sg_pumpshotgun`,
-    weapon_assaultrifle = `w_ar_assaultrifle`,
-    weapon_assaultrifle_mk2 = `w_ar_assaultrifle`,
-    weapon_carbinerifle = `w_ar_carbinerifle`,
-    weapon_carbinerifle_mk2 = `w_ar_carbinerifle`,
-    weapon_advancedrifle = `w_ar_advancedrifle`,
-    weapon_specialcarbine = `w_ar_specialcarbine`,
-    weapon_bullpuprifle = `w_ar_bullpuprifle`,
-    weapon_petrolcan = `prop_jerrycan_01a`,
-    pistol_ammo = `w_pi_pistol_mag1`,
-    rifle_ammo =    `w_ar_assaultrifle_mag1`,
-    smg_ammo =   `w_sb_assaultsmg_mag2`,
-    shotgun_ammo = `w_sg_assaultshotgun_mag1`,
-    mg_ammo =  `prop_box_ammo01a`,
-    snp_ammo  =  `prop_box_ammo01a`,
-    pistol_defaultclip = `w_pi_sns_pistol_mag1`,
-    pistol_extendedclip = `w_pi_sns_pistol_mag1`,
+
+      -- AR
+      weapon_assaultrifle = `w_ar_assaultrifle`,
+      weapon_assaultrifle_mk2 = `w_ar_assaultrifle`,
+      weapon_carbinerifle = `w_ar_carbinerifle`,
+      weapon_carbinerifle_mk2 = `w_ar_carbinerifle`,
+      weapon_advancedrifle = `w_ar_advancedrifle`,
+      weapon_specialcarbine = `w_ar_specialcarbine`,
+      weapon_bullpuprifle = `w_ar_bullpuprifle`,
+
+
+      --random weapon
+      weapon_petrolcan = `prop_jerrycan_01a`,
+
+
+      -- Ammo
+      pistol_ammo = `w_pi_pistol_mag1`,
+      rifle_ammo =    `w_ar_assaultrifle_mag1`,
+      smg_ammo =   `w_sb_assaultsmg_mag2`,
+      shotgun_ammo = `w_sg_assaultshotgun_mag1`,
+      mg_ammo =  `prop_box_ammo01a`,
+      snp_ammo  =  `prop_box_ammo01a`,
+      
+      
+      -- Pistol Attachments
+      pistol_defaultclip = `w_pi_sns_pistol_mag1`,
+      pistol_extendedclip = `w_pi_sns_pistol_mag1`,
+    --   pistol_flashlight = `w_pi_sns_pistol_mag1`,
     pistol_suppressor = `w_at_pi_supp`,
     pistol_luxuryfinish = `prop_paints_can05`,
     combatpistol_defaultclip = `w_pi_sns_pistol_mag1`,
@@ -464,6 +487,8 @@ Config.DropItems = {
     heavypistol_grip = `w_at_ar_afgrip`,
     vintagepistol_defaultclip = `w_pi_sns_pistol_mag1`,
     vintagepistol_extendedclip = `w_pi_sns_pistol_mag1`,
+
+    --SMG attachments
     microsmg_defaultclip = `w_pi_sns_pistol_mag1`,
     microsmg_extendedclip = `w_pi_sns_pistol_mag1`,
     microsmg_scope = `w_at_scope_medium`,
@@ -485,6 +510,8 @@ Config.DropItems = {
     combatpdw_drum = `w_mg_mg_mag1`,
     combatpdw_grip = `w_at_ar_afgrip`,
     combatpdw_scope = `w_at_scope_medium`,
+
+    -- RIFLE ATTACHMENTS
     assaultrifle_defaultclip = `w_ar_specialcarbine_mag2`,
     assaultrifle_extendedclip = `w_ar_specialcarbine_mag2`,
     assaultrifle_drum = `w_mg_mg_mag1`,
@@ -498,31 +525,45 @@ Config.DropItems = {
     carbinerifle_luxuryfinish = `prop_paints_can05`,
     combatpdw_scope = `w_at_scope_medium`,
     combatpdw_scope = `w_at_scope_medium`,
+
+      -- sniper
       weapon_sniperrifle = `w_sr_sniperrifle`,
       weapon_heavysniper = `w_sr_heavysniper`,
       weapon_marksmanrifle = `w_sr_marksmanrifle`,
       weapon_heavysniper_mk2 = `w_sr_heavysniper`,
       weapon_marksmanrifle_mk2 = `w_sr_marksmanrifle`,
+      
+      -- Thorwable
       weapon_molotov = `w_ex_molotov`,
       weapon_grenade = `w_ex_grenadefrag`,
       weapon_smokegrenade = `w_ex_grenadesmoke`,
+      
+      -- mat
       tosti = `prop_sandwich_01`,
       twerks_candy = `ng_proc_candy01a`,
       sandwich = `prop_sandwich_01`,
+      
+      -- drikke
       water_bottle = `prop_ld_flow_bottle`,
       coffee = `p_amb_coffeecup_01`,
       kurkakola = `ng_proc_sodacan_01a`,
       ecola = `ng_proc_sodacan_01a`,
       ecola = `sprunk`,
+      
+      -- alkohol
       beer = `prop_prop_beer_pissh`,
       whiskey = `prop_bottle_richard`,
       vodka = `prop_vodka_bottle`,
       wine = `prop_wine_bot_01`,
       grapejuice = `prop_food_bs_juice03`,
+
+      -- ID
       id_card = `p_ld_id_card_002`,
       driver_license = `prop_cs_swipe_card`,
       security_card_01 = `prop_cs_r_business_card`,
       security_card_02 = `prop_cs_r_business_card`,
+
+      -- drugs
       weed_skunk = `prop_weed_bottle`,
       cokebaggy = `prop_meth_bag_01`,
       meth = `prop_meth_bag_01`,
@@ -530,6 +571,8 @@ Config.DropItems = {
       cigar = `prop_sh_cigar_01`,
       rolling_paper = `p_cs_papers_01`,
       cigarette = `prop_cs_ciggy_01b`,
+
+      --materials
       plastic = `prop_cs_cardbox_01`,
       metalscrap = `prop_cs_cardbox_01`,
       copper = `prop_cs_cardbox_01`,
@@ -541,6 +584,8 @@ Config.DropItems = {
       rubber = `prop_cs_cardbox_01`,
       glass = `prop_cs_cardbox_01`,
       cloth = `prop_cs_cardbox_01`,
+
+      --tools
       lockpick = `prop_tool_screwdvr03`,
       advancedlockpick = `prop_tool_screwdvr03`,
       electronickit = `hei_prop_hst_usb_drive`,
@@ -550,32 +595,49 @@ Config.DropItems = {
       screwdriverset = `prop_tool_screwdvr03`,
       drill = `prop_tool_drill`,
       laserdrill = `hei_prop_heist_drill`,
+
+
+       --vehicle tools
        repairkit = `prop_tool_box_06`,
        advancedrepairkit = `prop_tool_box_06`,
        cleaningkit = `prop_rag_01`,
        jerry_can = `w_am_jerrycan`,
+   
+       -- Medication
        firstaid = `prop_cs_pills`,
        bandage = `prop_cs_pills`,
        ifaks = `prop_cs_pills`,
        painkillers = `prop_cs_pills`,
        walkstick = `prop_cs_walking_stick`,
+
+       -- Communication
+
        phone = `lb_phone_prop`,
        radio = `prop_cs_walkie_talkie`,
+    
+       -- Theft and Jewelry
        rolex = `p_watch_04`,
        diamond_ring = `prop_jewel_03b`,
        goldchain = `p_jewel_necklace01_s`,
        goldbar = `hei_prop_heist_gold_bar`,
        radio = `prop_cs_walkie_talkie`,
        radio = `prop_cs_walkie_talkie`,
+
+       -- Cops Tools
        armor = `prop_bodyarmour_03`,
        heavyarmor = `prop_bodyarmour_03`,
        empty_evidence_bag = `prop_meth_bag_01`,
        filled_evidence_bag = `prop_meth_bag_01`,
+
+       -- Other Tools
        stickynote = `p_notepad_01_s`,
        parachute = `p_parachute_s`,
        binoculars = `prop_binoc_01`,
        lighter = `p_cs_lighter_01`,
        markertesedler = `prop_anim_cash_pile_01`,
+
+
+       -- Ransutstyr
        juvelen_bakrom = `prop_cs_r_business_card`,
        robbery_keycard_01 = `prop_cs_r_business_card`,
        robbery_keycard_02 = `prop_cs_r_business_card`,
@@ -584,10 +646,14 @@ Config.DropItems = {
        robbery_keycard_05 = `prop_cs_r_business_card`,
        robbery_keycard_06 = `prop_cs_r_business_card`,
        robbery_keycard_07 = `prop_cs_r_business_card`,
+
+       -- jaksam
        doors_lockpick = `prop_tool_screwdvr03`,
        storbank_kort = `prop_cs_r_business_card`,
        hvelv_kort = `prop_cs_r_business_card`,
        eksplosiv = `prop_bomb_01`,
+
+       -- Random
        cups = `prop_plastic_cup_02`,
        contract = `prop_cd_paper_pile3`,
        flyid = `prop_cs_swipe_card`,
@@ -601,10 +667,17 @@ Config.DropItems = {
        security_card_04 = `prop_cs_r_business_card`,
        spray = `prop_spraygun_01`,
        spray = `prop_rag_01`,
+
+       --kq
        kq_outfitbag = `prop_cs_heist_bag_01`,
+
        towing_rope = `prop_trevor_rope_01`,
+
+      -- sykehus
       crutch = `prop_cs_walking_stick`,
       wheelchair = `prop_wheelchair_01`,
+
+      -- houserobbery
       powder = `prop_meth_bag_01`,
       watch = `p_watch_04`,
       toothpaste = `prop_toothpaste_01`,
@@ -641,15 +714,27 @@ Config.DropItems = {
       hack_laptop = `prop_laptop_02_closed`,
       toiletry = `prop_toilet_brush_01`,
       skull = `p_jewel_pickup33_s`,
+
+      -- mask
       mask = `prop_mask_test_01`,
+ 
+      --falsk legg
       info_usb = `hei_prop_hst_usb_drive`,
       clean_card = `p_ld_id_card_002`,
+
+
+        --jaksam robberies creator
         hacking_computer = `prop_laptop_02_closed`,
         thermal_charge = `hei_prop_heist_thermite`,
         gas_mask = `prop_mask_test_01`,
         drill = `prop_tool_drill`,
         gold_ingot = `hei_prop_heist_gold_bar`,
+
+
+        -- KamaKaZibO
         tablet = `prop_cs_tablet_02`,
+
+        --tequilala
         appledrink = `prop_drink_whisky`,
         bananadrink = `prop_drink_whisky`,
         orangedrink = `prop_drink_whisky`,
@@ -675,14 +760,20 @@ Config.DropItems = {
         orange = `prop_bar_fruit`,
         daquiri = `prop_bar_fruit`,
         tracker = `hei_prop_hst_usb_drive`,
+
+        -- ps-drugprocessing
         methtray = `prop_chem_vial_02`,
         coke = `prop_coke_block_01`,
         marijuana = `prop_weed_02`,
+
+        -- GGC Custom Weapons -- Melees
         weapon_katana = `w_me_katana`,
         weapon_shiv = `w_me_shiv`,
         weapon_sledgehammer = `prop_tool_sledgeham`,
         weapon_karambit = `w_me_karambit`,
         weapon_keyboard = `w_me_keyboard`,
+
+        -- GGC Custom Weapons -- Hand Guns
         weapon_glock17 = `w_pi_glock17_luxe`,
         weapon_glock18c = `w_pi_glock18c`,
         weapon_glock22 = `w_pi_glock22`,
@@ -691,8 +782,12 @@ Config.DropItems = {
         weapon_m1911 = `w_pi_m1911`,
         weapon_glock20 = `glock20`,
         weapon_glock19gen4 = `glock19gen4`,
+
+          -- GGC Custom Weapons -- SMGs
           weapon_pmxfm = `pmx`,
           weapon_mac10 = `w_sb_mac10`,
+
+          -- GGC Custom Weapons -- Rifles
           weapon_mk47fm = `MK47FluffysMods`,
           weapon_m6ic = `m6ic`,
           weapon_scarsc = `scarsc`,
@@ -702,6 +797,8 @@ Config.DropItems = {
           weapon_aks74 = `w_ar_assaultrifle_aks74`,
           weapon_groza = `w_ar_groza`,
           weapon_scarh = `w_ar_scarh`,
+
+          -- Airsoft
           weapon_airsoftak47 = `airsoftak`,
           weapon_airsoftg36c = `airsoftg36`,
           weapon_airsoftglock20 = `airsoftglock20`,
@@ -711,15 +808,20 @@ Config.DropItems = {
           weapon_airsoftmp5 = `airsoftmp5`,
           weapon_airsoftr700 = `airsoftr700`,
           weapon_airsoftr870 = `airsoft870`,
+
+
+          --R14 police
           nikon = `prop_pap_camera_01`,
           microfibercloth = `prop_rag_01`,
+
           basketball = `prop_bskball_01`,
           basketball_hoop = `stt_prop_hoop_small_01`,
+
           teddy = `v_ilev_mr_rasberryclean`,
           bunch_of_flowers = `prop_snow_flower_02`,
           rose = `prop_single_rose`,
+
           smalltv = `prop_tv_01`,
           mediumtv = `prop_tv_flat_03b`,
           bigtv = `prop_tv_flat_michael`,
 }
-
